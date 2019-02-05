53cfe4ef5d341a1662e8aae4f8901848cb34474c
alumno <alumno@PC-XX>
2019-02-05 14:52:59 +0100

    modify

--- DUO/src/DUO.java
+++ DUO/src/DUO.java
@@ -4,6 +4,7 @@ public class DUO {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Ivan");
+		System.out.println("Hola Antonio");
 	}
 
 }
